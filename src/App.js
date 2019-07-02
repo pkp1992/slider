e95c70bb363201ed253c6f1baf06d81a7e1a5bef
pkp1992 <pkp1992-92@mail.ru>
2019-07-02 17:22:05 +0300

    Menu

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Button from "react-bootstrap/Button";
-import styled from 'styled-components'
+import Col from "react-bootstrap/Col";
+import styled from "styled-components";
+import Header from "./components/Header";
+import Menu from "./components/Menu";
+import Main from "./components/Main";
 import "./App.css";
+
+const url = process.env.PUBLIC_URL + "/img/bg.jpg";
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  /* height: 49px; */
+  background-color: #505050;
+  opacity: 0.8;
+`;
+
+const MenuWrapper = styled.div`
+  width: 100%;
+  height: 89px;
+  padding-top: 20px;
+  background-color: #fff;
+`;
+const MainWrapper = styled.main`
+  height: 600px;
+  padding-top: 130px;
+  background: url(${url}) no-repeat;
+  background-size: cover;
+`;
 
 function App() {
   return (
-    <Container>
-      <Row>
-        <Button>Click</Button>
-        <p>Hello</p>
-      </Row>
-    </Container>
+    <div className="App">
+      <HeaderWrapper>
+        <Container>
+          <Header />
+        </Container>
+      </HeaderWrapper>
+      <MenuWrapper>
+        <Container>
+          <Menu />
+        </Container>
+      </MenuWrapper>
+      <MainWrapper>
+        <Container>
+          <Main></Main>
+        </Container>
+      </MainWrapper>
+    </div>
   );
 }
 
